71a16570438fb0e97ff52614ee09beb166b6d30a
menomanabdulla <noman.fcibappy@gmail.com>
2019-01-10 15:22:06 +0600

    chaining block

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const sha26 = require('crypto-js/sha256');
 
 class Block{
-    constructor(timestamp,data,previousHash){
+    constructor(timestamp,data,previousHash = ''){
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
@@ -12,6 +12,30 @@ class Block{
     }
 }
 
-const block = new Block("2019-01-01",{amount: 5},"ABCD");
+class Blockchain {
+    constructor(){
+        this.chain = [this.generateGenesisBlock()];
+    }
 
-console.log(block);
+    generateGenesisBlock(){
+        return new Block("2019-01-01","GENESIS","0000");
+    }
+
+    getLatestBLock(){
+        return this.chain[this.chain.length-1];
+    }
+
+    addBlock(newBLock){
+        newBLock.previousHash = this.getLatestBLock().hash;
+        newBLock.hash = newBLock.calculateHash();
+        this.chain.push(newBLock);
+    }
+}
+
+const josscoin = new Blockchain();
+
+const block = new Block("2019-01-01",{amount: 5});
+josscoin.addBlock(block);
+
+
+console.log(josscoin);
